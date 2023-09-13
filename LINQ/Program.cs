63cfa55ee3d26f357ecf9a6733c0f29da9497111
FrankAlves11 <Frankie.alves@icloud.com>
2023-09-13 09:56:01 -0700

    made changes

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            List<string> videoGames = new List<string>()
+            {
+                "Skyrim",
+                "Fallout New Vegas",
+                "Minecraft",
+                "Apex Legends",
+                "Call of duty black ops",
+                "Outlast"
+            };
+
+            var orderedGames = videoGames.OrderBy(game => game.Length);
+
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
+
+        }      
+       
     }
 }
